267433746cf796314b1d4c6648dede82d4f556fb
Sai Lami <lami_s@hotmail.it>
2025-02-24 16:43:36 +0100

    weld_spot setter

--- src/comau_model/weld_spot.py
+++ src/comau_model/weld_spot.py
@@ -9,18 +9,18 @@ class WeldSpot(ComauMove):
     @property
     def spot_index(self) -> int:
         if self.condition:
-            for condition in self.condition:
-                if "spot" in condition:
-                    condition_params: str = (
-                        condition.split("(")[1].split(")")[0].strip()
-                    )
-
-                    if condition_params.isnumeric():
-                        return int(condition_params.strip())
-
-                    return int(self._extract_index_from_params(condition_params))
-
+            if condition := self._find_spot_condition():
+                return self._index_from_condition(condition)
         return 0
+
+    @spot_index.setter
+    def spot_index(self, value: int) -> None:
+        if self.condition:
+            if condition := self._find_spot_condition():
+                condition.replace(str(self.spot_index), str(value))
+
+    def change_index(self, new_index: int) -> None:
+        self.spot_index = new_index
 
     def __repr__(self) -> str:
         return f"  MOVE {self.move_type.name} TO {self.name}, \n    {'\n   '.join(self.condition)} \n  ENDMOVE"
@@ -37,6 +37,18 @@ class WeldSpot(ComauMove):
 
     def set_conventional_name(self) -> str:
         return self.name if self.is_name_conventional() else f"WP_{self.spot_index}"
+
+    def _find_spot_condition(self) -> str:
+        for condition in self.condition:
+            if "spot" in condition:
+                return condition
+        return ""
+
+    def _index_from_condition(self, spot_condition: str) -> int:
+        condition_params: str = spot_condition.split("(")[1].split(")")[0].strip()
+        if condition_params.isnumeric():
+            return int(condition_params.strip())
+        return int(self._extract_index_from_params(condition_params))
 
     def _extract_index_from_params(self, params: str) -> int:
         for param in params.split(","):
